3ae3fd0ef5051cbe25d11ad21d9e18baed180c13
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-23 18:42:32 +0100

    created nav

---
+++ src/pages/Hero.tsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <div>
+      <div>
+         <h1>The web3 platform
+         for community-driven merchandise</h1>
+         <p>CoinSwag  is  a Decentalized platform tha enables Crypto cummunities to</p>
+         <p>effortlessly create stores, make profits from merchandise with the use of Blockchain technologies. </p>
+         <button>Create Store</button>
+      </div>
+    </div>
+  )
+}
+export default Hero
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/landing-page/NavBar"
+
+function LandingPage() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+export default LandingPage
