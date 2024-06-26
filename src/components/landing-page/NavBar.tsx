c6014802209649f4567d4ded45560101cd15d7fc
Uba chukwuka promise <promiseuba67@gmail.com>
2024-06-26 07:21:30 +0100

    add create account button

--- src/components/landing-page/NavBar.tsx
+++ src/components/landing-page/NavBar.tsx
@@ -1,15 +1,18 @@
+import { ConnectAccount } from "@coinbase/onchainkit/wallet";
+
 function NavBar() {
-	return (
-		<nav className="fixed top-0 left-0 h-[10vh] flex justify-between items-center w-full px-8 bg-white shadow-xl z-10">
-			<span className="flex justify-center items-center text-primary font-black uppercase text-xl">
-				Coinswag
-			</span>
-			<div className="[&>a]:text-sm gap-10 flex items-center">
-				<a href="">Home</a>
-				<a href="">Merchandise</a>
-				<a href="">About</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="fixed top-0 left-0 h-[10vh] flex justify-between items-center w-full px-8 bg-white shadow-xl z-10">
+      <span className="flex justify-center items-center text-primary font-black uppercase text-xl">
+        Coinswag
+      </span>
+      <div className="[&>a]:text-sm gap-10 flex items-center">
+        <a href="">Home</a>
+        <a href="">Merchandise</a>
+        <a href="">About</a>
+        <ConnectAccount />
+      </div>
+    </nav>
+  );
 }
 export default NavBar;
