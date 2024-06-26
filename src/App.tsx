712d508eff52b5f3d9fc0cb674377cbbe21f0141
Uba chukwuka promise <promiseuba67@gmail.com>
2024-06-26 06:09:16 +0100

    -ready for commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,43 @@ import AuthLayout from "./pages/auth/auth-layout/AuthLayout";
 import Login from "./pages/auth/auth-section/Login";
 import Register from "./pages/auth/auth-section/Register";
 import OnchainProviders from "./providers/onchainkig-provider";
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <LandingPage />,
-	},
-	{
-		path: "/dashboard",
-		element: <DashboardLayout />,
-	},
-	{
-		element: <AuthLayout />,
-		children: [
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/register",
-				element: <Register />,
-			},
-			{
-				path:"/new-store"
-			}
-		],
-	},
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/new-store",
+      },
+    ],
+  },
 ]);
 
 function App() {
   return (
     <Fragment>
-      <Wagmi>
+      <OnchainProviders>
         <RouterProvider router={router} />
       </OnchainProviders>
     </Fragment>
-	);
+  );
 }
 
 export default App;
